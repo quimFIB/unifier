2e64cc1a6d045e9f06cf8f7663ec8400a76d777a
quim <quim@local>
2020-06-15 00:08:53 +0200

    [WIP] Equality set

--- base.ml
+++ base.ml
@@ -5,12 +5,10 @@ print_string "Hello World!\n"
 module Term =
   struct
     type term =
-      | Bottom
       | Atom of string
       | Term of string * term list
 
     let rec str (t : term) = match t with
-      | Bottom -> "_|_"
       | Atom s -> s
       | Term (s,l) -> s ^ "(" ^ (String.concat "," (List.map str l)) ^ ")"
     (* let rec sub (a : term) (sub : term) (t : term) = true *)
@@ -24,13 +22,8 @@ let t3 = Term.Term ("f",[Atom "c"])
 module Unifier =
   struct
     type equality = Equal of Term.term * Term.term
-    let rec sub (a : Term.term) (sub_t : Term.term) (t : Term.term) : Term.term option = match a, t with
-      | Atom x, Atom y -> if x = y then Some sub_t else Some (Atom x)
-      (* | Atom x, Term (f,l) -> let result = List.map (fun x -> Option.to_list (sub x)) l in
-       *    if List.exists (fun t -> match t with Some t -> false | None -> true) result = true then
-       *      None
-       *    else
-       *      Some (Term (f, result)) *)
-      | Atom x, Term (f,l) -> Some (Term (f, (List.flatten (List.map Option.to_list (List.map (sub a sub_t) l)))))
-      | _ -> None
+    let reflex (eq : equality) = match eq with Equal (t1,t2) -> Equal (t2,t1)
+    let sub (equal : equality) (t : Term.term) : Term.term =
+      match equal with
+      | Equal (t_l,t_r) -> if t_l = t then t_r else t
 end

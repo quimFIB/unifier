3424ecb36be0b06721d3eb8ccca9ec7af265160b
quim <quim@local>
2020-06-24 12:50:06 +0200

    Add subtyping

--- my_unification_alg.ml
+++ my_unification_alg.ml
@@ -1,28 +1,34 @@
-(* A small OCaml program *)
-print_string "Hello World!\n"
-
+(* Robinson Unification Algorithm *)
+module Var =
+  struct
+    type var = [`Var of string]
+    type t = var
+    let compare (v1 : var) (v2 : var) = compare v1 v2
+  end
 module Term =
   struct
     type term =
-      | Var of string
-      | Term of string * term list
+      [
+        `Var of string
+      | `Term of string * term list
+      ]
     type t = term
     let compare (t1 : term) (t2 : term) = compare t1 t2
     let rec str (t : term) = match t with
-      | Var s -> s
-      | Term (s,l) -> s ^ "(" ^ (String.concat "," (List.map str l)) ^ ")"
+      | `Var s -> s
+      | `Term (s,l) -> s ^ "(" ^ (String.concat "," (List.map str l)) ^ ")"
     (* let rec sub (a : term) (sub : term) (t : term) = true *)
-    (* let rec sub (Var a) (sub : term) (t : term) = true *)
+    (* let rec sub (`Var a) (sub : term) (t : term) = true *)
 end
 
-let t1 = Term.Term ("f",[Var "a"])
-let t2 = Term.Term ("g",[Var "b"])
-let t3 = Term.Term ("f",[Var "c"])
-let t4 = Term.Term ("z",[Var "a"])
-let t5 = Term.Term ("w",[Var "c"])
+let t1 = Term.(`Term ("f",[`Var "a"]))
+let t2 = Term.(`Term ("g",[`Var "b"]))
+let t3 = Term.(`Term ("f",[`Var "c"]))
+let t4 = Term.(`Term ("z",[`Var "a"]))
+let t5 = Term.(`Term ("w",[`Var "c"]))
 
 
-module VarSet = Set.Make(Term)
+module VarSet = Set.Make(Var)
 module VarIntMap = Map.Make(Term)
 module Unifier =
   struct
@@ -34,17 +40,17 @@ module Unifier =
     let rec sub (equal : equality) (t : Term.term) : Term.term =
       match equal with
       | Equal (t_l,t_r) -> if t_l = t then t_r else match t with
-                                                    | Term (f,l) -> Term (f, List.map (sub equal) l)
+                                                    | `Term (f,l) -> `Term (f, List.map (sub equal) l)
                                                     | _ -> t
-    let rec occurs (t : Term.term) (v : Term.term) = match v,t with
-      | Var x, Term (f, l) -> List.exists (occurs v) l
-      | _,_ -> false
+    let rec occurs (v : Var.var) (t : Term.term) = match t with
+      | `Term (f, l) -> List.exists (occurs v) l
+      | _ -> t = (v :> Term.term)
     let conflict (t1: Term.term) (t2: Term.term) = match t1, t2 with
-      | Term (f,l1), Term (g,l2) -> (f <> g) or (List.length l1 <> List.length l2)
+      | `Term (f,l1), `Term (g,l2) -> (f <> g) or (List.length l1 <> List.length l2)
       | _ -> false
     let decomposable (eq: equality) = match eq with Equal (t1,t2) ->
                                                      match t1,t2 with
-                                                     | Term (f, l_f), Term (g, l_g) ->
+                                                     | `Term (f, l_f), `Term (g, l_g) ->
                                                         if (f = g) & (List.length l_f = List.length l_g) then
                                                           true
                                                         else
@@ -53,11 +59,11 @@ module Unifier =
 
     let decompose (eq: equality) = match eq with Equal (t1,t2) ->
                                                   match t1,t2 with
-                                                  | Term (f, l_f), Term (g, l_g) -> List.rev_map2 (fun x y -> Equal (x,y)) l_f l_g
+                                                  | `Term (f, l_f), `Term (g, l_g) -> List.rev_map2 (fun x y -> Equal (x,y)) l_f l_g
                                                   | _,_ -> []
     let rec getVarsTerm (t : Term.term) = match t with
-      | Var x -> VarSet.(empty |> add (Var x))
-      | Term (f, l)-> List.fold_left VarSet.union VarSet.empty (List.map getVarsTerm l)
+      | `Var x -> VarSet.(empty |> add (`Var x))
+      | `Term (f, l)-> List.fold_left VarSet.union VarSet.empty (List.map getVarsTerm l)
 
     let getVarsEq (eq : equality) = match eq with Equal (t1,t2) -> VarSet.union (getVarsTerm t1) (getVarsTerm t2)
   end
@@ -79,11 +85,11 @@ module Unify =
     let unify {vars : VarSet.t;  eqSet : EqSet.t} = let eq0 = EqSet.choose eqSet in
                                   match eq0 with
                                     Equal (t1,t2) -> match t1, t2 with
-                                                     | Term (f, l), Var x -> EqSet.(remove eq0 eqSet |> add (Unifier.reflex eq0))
-                                                     (* | Var x, Term (f, l) -> EqSet.(map (Unifier.sub t2) eqSet |> ) *)
+                                                     | `Term (f, l), `Var x -> EqSet.(remove eq0 eqSet |> add (Unifier.reflex eq0))
+                                                     | `Var x, `Term (f, l) -> if Unifier.occurs (`Var x) t2 then EqSet.empty
+                                                                               else eqSet
                                                      | _,_ -> if t1 = t2 then EqSet.remove eq0 eqSet
                                                               else if Unifier.decomposable eq0 then EqSet.(remove eq0 eqSet |> union (of_list (Unifier.decompose eq0)))
-                                                              else if Unifier.occurs t1 t2 then EqSet.empty (*we have to fail so return empty set for the time being*)
                                                               else eqSet
   end
 let pretty_print t = List.fold_left (fun x y -> x ^ y) "" (List.map (fun x -> (Unifier.str x) ^ ";") (EqSet.elements t))
@@ -93,7 +99,7 @@ let eq2 = Unifier.Equal (t2, t2)
 let eq3 = Unifier.Equal (t3, t1)
 let eq4 = Unifier.Equal (t3, t4)
 let eq5 = Unifier.Equal (t1, t5)
-let eqSub = Unifier.Equal (Var "c", Term ("y", [Var "c0"; Var "c1"]))
+let eqSub = Unifier.Equal (`Var "c", `Term ("y", [`Var "c0"; `Var "c1"]))
 let foo = EqSet.(empty |> add eq1 |> add eq2 |> add eq3 |> add eq4 |> add eq5)
 
 (* "Pretty" print the EqSet elements *)

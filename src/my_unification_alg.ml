47e22b587a659d908a45e38a07759f78a4c007a0
quim <quim.casals@gmail.com>
2020-06-28 14:11:51 +0200

    [WIP] Need to implement equality substitution

--- src/my_unification_alg.ml
+++ src/my_unification_alg.ml
@@ -74,10 +74,13 @@ module UniSet =
     type t = {vSet : VarMSet.t; eqSet : EqSet.t;}
     let add (eq : Unifier.equality) (uniSet : t) = let found = EqSet.find_opt eq uniSet.eqSet in
                             match found with None -> {vSet = VarMSet.union uniSet.vSet (Unifier.getVarsEq eq); eqSet = EqSet.add eq uniSet.eqSet} | _ -> uniSet
-    (* TODO: remove equality from set *)
+    let add_from_list (eq_list : Unifier.equality list) (uniSet : t) = List.fold_left (fun s x -> add x s) uniSet eq_list
     let remove (eq : Unifier.equality) (uniSet : t) = let found = EqSet.find_opt eq uniSet.eqSet in
                                match found with None -> uniSet | Some eq0 -> let vars = Unifier.getVarsEq eq0 in
                                                       {vSet = VarMSet.diff uniSet.vSet vars ; eqSet = EqSet.remove eq0 uniSet.eqSet}
+    let contains (x : Var.var) (uniSet: t) = VarMSet.mem (x,0) uniSet.vSet
+    let empty = {vSet = VarMSet.empty; eqSet = EqSet.empty}
+    (* let substitute (eq_sub : Unifier.equality) (uniSet: t) = match eq_sub with Equal (){vSet = VarMSet.remove x uniSet.vSet; eqSet = EqSet.map (fun eq -> match eq with Equal (t1,t2) -> Equal (Unifier.sub eq_sub t1, Unifier.sub eq_sub t2)) uniSet.eqSet} *)
 
   end
 module Unify =
@@ -85,15 +88,25 @@ module Unify =
   include UniSet
     let eliminate (eq : Unifier.equality) (eqSet : EqSet.t) = match eq with
       | Equal (t1, t2) -> EqSet.(map (fun x -> match x with Equal (t1,t2) -> Equal (Unifier.sub eq t1,Unifier.sub eq t2)) eqSet |> EqSet.add eq)
-    let unify (set : UniSet.t) = let eq0 = EqSet.choose set.eqSet in
+    (*occur check. If given equality of the form x = f(s0,...,sk) checking that for all si, x =/= si*)
+    let occurs_rec (eq : Unifier.equality) = match eq with Equal (t1,t2) -> match t1, t2 with
+                                                                          | `Var x, `Term (f, l) -> Unifier.occurs (`Var x) t2
+                                                                          | _, _ -> false
+    let conflict (eq : Unifier.equality) = match eq with Equal (t1,t2) -> Unifier.conflict t1 t2
+    let unify (set, candidates) : UniSet.t * EqSet.t = let eq0 = EqSet.choose candidates in
                                   match eq0 with
                                     Equal (t1,t2) -> match t1, t2 with
-                                                     | `Term (f, l), `Var x -> EqSet.(remove eq0 set.eqSet |> add (Unifier.reflex eq0))
-                                                     | `Var x, `Term (f, l) -> if Unifier.occurs (`Var x) t2 then EqSet.empty
-                                                                               else set.eqSet
-                                                     | _,_ -> if t1 = t2 then EqSet.remove eq0 set.eqSet
-                                                              else if Unifier.decomposable eq0 then EqSet.(remove eq0 set.eqSet |> union (of_list (Unifier.decompose eq0)))
-                                                              else set.eqSet
+                                                     | `Term (f, l), `Var x -> (UniSet.(remove eq0 set|> add (Unifier.reflex eq0)),EqSet.(remove eq0 set.eqSet |> add (Unifier.reflex eq0))) (*flip or swap*)
+                                                     | `Var x, `Term (f, l)  -> let contains = UniSet.contains (`Var x) set
+                                                                                and occurs = (Unifier.occurs (`Var x) (`Term (f, l))) in
+                                                         if  contains && not occurs then (set, EqSet.remove eq0 candidates)
+                                                         else if not contains then (set, EqSet.remove eq0 candidates)
+                                                         else (UniSet.empty, EqSet.empty)
+                                                     | _,_ -> if t1 = t2 then (UniSet.remove eq0 set, EqSet.remove eq0 candidates) (* Delete rule *)
+                                                              (* Decompose rule *)
+                                                              else if Unifier.decomposable eq0 then
+                                                                let new_eqs_list = Unifier.decompose eq0 in (UniSet.add_from_list new_eqs_list set, EqSet.union (EqSet.of_list new_eqs_list) candidates)
+                                                              else (UniSet.empty, EqSet.empty)
   end
 let pretty_print t = List.fold_left (fun x y -> x ^ y) "" (List.map (fun x -> (Unifier.str x) ^ ";") (EqSet.elements t))
 
